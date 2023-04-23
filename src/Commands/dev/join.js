6699eee545de852b86fa7ea3b0df4a35481587fc
ToxiPanda <121240392+ToxiPanda@users.noreply.github.com>
2023-04-23 19:23:00 +0530

    Update join.js

--- src/Commands/dev/join.js
+++ src/Commands/dev/join.js
@@ -5,12 +5,12 @@ module.exports = {
     exp: 0,
     description: 'Bot joins the group using the link',
     async execute(client, arg, M) {
-        if (!arg) return M.reply('Sorry you did not give any group link!')
-        if (!arg.includes('whatsapp.com')) return M.reply('Sorry you did not give any valid group link!')
+        if (!arg) return M.reply('Sorry you did not give any *Group* link!')
+        if (!arg.includes('whatsapp.com')) return M.reply('Sorry you did not give any *valid* group link!')
         const JoinCode = arg.split('https://chat.whatsapp.com/')[1]
         client
             .groupAcceptInvite(JoinCode)
             .then((res) => M.reply('Joined'))
-            .catch((res) => M.reply('Something went wrong please check the link'))
+            .catch((res) => M.reply('Something went wrong please *check* the link'))
     }
 }
