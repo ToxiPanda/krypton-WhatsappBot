5ba5737eae164e9337f33ff9c8cd59ff4077a098
ToxiPanda <121240392+ToxiPanda@users.noreply.github.com>
2023-04-23 19:29:34 +0530

    Update delete.js

--- src/Commands/general/delete.js
+++ src/Commands/general/delete.js
@@ -5,7 +5,7 @@ module.exports = {
     exp: 5,
     description: 'Deletes the quoted message',
     async execute(client, arg, M) {
-        if (!M.quoted) return M.reply('Quote the message that you want me to delete, Baka!')
+        if (!M.quoted) return M.reply('Quote the message that you want me to *delete*')
         await client.sendMessage(M.from, {
             delete: M.quoted.key
         })
